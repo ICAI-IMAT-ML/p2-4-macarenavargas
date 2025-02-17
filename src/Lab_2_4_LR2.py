9f4c9391ae194b7f343831a2684a87e35ac10217
macarenavargas <macarena.vargashiguero@gmail.com>
2025-02-17 17:10:41 +0100

    test y notebook done

--- src/Lab_2_4_LR2.py
+++ src/Lab_2_4_LR2.py
@@ -64,11 +64,19 @@ class LinearRegressor:
         Returns:
             None: Modifies the model's coefficients and intercept in-place.
         """
-        # Replace this code with the code you did in the previous laboratory session
+        # X sera algo como:
+        # X = np.array([1,2,3],[1,4,5],[1,8,9],[1,4,3]) donde la columna de unos es el bias
+        # y las otras dos columnas son los features x1 y x2
 
-        # Store the intercept and the coefficients of the model
-        self.intercept = None
-        self.coefficients = None
+        # y sera algo como y= np.array([1,2,3,4,5])
+
+        X_t=np.transpose(X)
+        X_tX=np.dot(X_t,X)
+        w=np.dot(np.dot(np.linalg.inv(X_tX),X_t),y)
+        b=w[0]
+        w=w[1:]
+        self.intercept = b
+        self.coefficients = w
 
     def fit_gradient_descent(self, X, y, learning_rate=0.01, iterations=1000):
         """
@@ -85,34 +93,45 @@ class LinearRegressor:
         """
 
         # Initialize the parameters to very small values (close to 0)
+        # tus observaciones qu quieres fit tienen la pinta (x_i,y_i)
         m = len(y)
         self.coefficients = (
             np.random.rand(X.shape[1] - 1) * 0.01
         )  # Small random numbers
         self.intercept = np.random.rand() * 0.01
+        
 
         # Implement gradient descent (TODO)
         for epoch in range(iterations):
-            predictions = None
+            
+            predictions = self.predict(X[:,1:])
             error = predictions - y
+            # para w
+           
+            grad_C_w=(2/m)*sum(error.reshape(-1, 1)*X[:,1:])
+         
+          
+
+            # para b
+            grad_C_b=(2/m)*sum(error) # como si lo estivieras multiplicando por un array de 1s
 
             # TODO: Write the gradient values and the updates for the paramenters
-            gradient = None
-            self.intercept -= None
-            self.coefficients -= None
+            gradient = (grad_C_w,grad_C_b)
+
+            self.intercept -= learning_rate* gradient[1]
+            self.coefficients -= learning_rate* gradient[0]
 
             # TODO: Calculate and print the loss every 10 epochs
             if epoch % 1000 == 0:
-                mse = None
+                mse = 1/len(y)*sum(error**2)
                 print(f"Epoch {epoch}: MSE = {mse}")
-
     def predict(self, X):
         """
         Predict the dependent variable values using the fitted model.
 
         Args:
             X (np.ndarray): Independent variable data (1D or 2D array).
-            fit (bool): Flag to indicate if fit was done.
+            
 
         Returns:
             np.ndarray: Predicted values of the dependent variable.
@@ -120,13 +139,25 @@ class LinearRegressor:
         Raises:
             ValueError: If the model is not yet fitted.
         """
-
-        # Paste your code from last week
-
         if self.coefficients is None or self.intercept is None:
             raise ValueError("Model is not yet fitted")
 
-        return None
+        # X son nuevos datos de entrada
+        # Si X es multidmensional sera ej:
+        # X = np.array([[1, 2], [3, 4], [5, 6]]) 3 observaciones, 2 features x1 x2
+        # si X es unidimensional sera:
+        if np.ndim(X) == 1:
+            X = X.reshape(-1, 1)
+               
+        # Multiplicamos nuestra matriz X por la lista de coefcientes
+        # Ej, self.coeficientes= np.array([2.5,3]) se multiplicaria cada
+        # lista de X por su elemento correspondiente
+        # como X tiene el bias, hacemos un slice
+        predictions = np.dot(X,self.coefficients) +self.intercept
+
+        # necesitamos el flatten porque en el caso de una dimension necesitamos volver aconvertirlo en un array de una lista
+    
+        return predictions.flatten()
 
 
 def evaluate_regression(y_true, y_pred):
@@ -142,19 +173,21 @@ def evaluate_regression(y_true, y_pred):
     """
 
     # R^2 Score
-    # TODO
-    r_squared = None
+    
+    RSS= np.sum((y_true-y_pred)**2)
+    TSS= np.sum((y_true-np.mean(y_true))**2)
+    r_squared = 1- RSS/TSS
 
     # Root Mean Squared Error
-    # TODO
-    rmse = None
+
+    n=y_pred.shape
+    rmse = np.sqrt(np.sum(((y_pred-y_true)**2)/n))
 
     # Mean Absolute Error
-    # TODO
-    mae = None
+
+    mae = np.sum(abs(y_true-y_pred)/n)
 
     return {"R2": r_squared, "RMSE": rmse, "MAE": mae}
-
 
 def one_hot_encode(X, categorical_indices, drop_first=False):
     """
@@ -170,21 +203,40 @@ def one_hot_encode(X, categorical_indices, drop_first=False):
         np.ndarray: Transformed array with one-hot encoded columns.
     """
     X_transformed = X.copy()
+    
     for index in sorted(categorical_indices, reverse=True):
         # TODO: Extract the categorical column
-        categorical_column = None
-
+        categorical_column = X[:, index]
+      
         # TODO: Find the unique categories (works with strings)
-        unique_values = None
+        unique_values = np.unique(categorical_column)
 
         # TODO: Create a one-hot encoded matrix (np.array) for the current categorical column
-        one_hot = None
+        matrix=[]
+        for value in unique_values:
+           
+            column=[]
+            for value_instance in categorical_column:
+                if value==value_instance:
+                    
+                    column.append(1)
+                else:
+                    column.append(0)
+            matrix.append(column)
+      
+      
 
+        one_hot = np.array(matrix).T
+        
         # Optionally drop the first level of one-hot encoding
         if drop_first:
             one_hot = one_hot[:, 1:]
 
         # TODO: Delete the original categorical column from X_transformed and insert new one-hot encoded columns
-        X_transformed = None
+        X_transformed=np.delete(X_transformed,index,axis=1)
+     
+        X_transformed=np.concatenate((one_hot,X_transformed), axis=1)
+
+        
 
     return X_transformed
